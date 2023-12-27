613b290100e4aecef9a2fde52fa081139e198cd1
geoffreychen777 <geoffreychen777@gmail.com>
2023-12-27 20:44:39 +0000

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,36 @@
-import { PLAPI, PLExtAPI } from "paperlib";
+import { PLAPI, PLExtAPI, PLExtension, PLMainAPI } from "paperlib-api";
 
-import { PLExtension } from "@/models/extension";
-import { EntryScrapeService } from "@/services/entry-scrape-service";
-
-class PaperlibEntryScrapeExtension extends PLExtension {
+class PaperlibHelloworldExtension extends PLExtension {
   disposeCallbacks: (() => void)[];
-
-  private readonly _entryScrapeService: EntryScrapeService;
 
   constructor() {
     super({
-      id: "paperlib-entry-scrape-extension",
-      name: "Entry Scrapers",
-      description: "The entry scrape extension for PaperLib.",
-      author: "Paperlib",
-      defaultPreference: {},
+      id: "@future-scholars/paperlib-demo-helloworld-extension",
+      defaultPreference: {
+        msg: {
+          type: "string",
+          name: "Message",
+          description: "Message to show when echo",
+          value: "Hello from the extension process",
+          order: 0,
+        },
+        signature: {
+          type: "boolean",
+          name: "Signature",
+          description: "Show signature in the message",
+          value: false,
+          order: 1,
+        },
+        lang: {
+          type: "options",
+          name: "Language",
+          description: "Language of the message",
+          options: { en: "English", zh: "Chinese" },
+          value: "en",
+          order: 2,
+        },
+      },
     });
-
-    this._entryScrapeService = new EntryScrapeService();
 
     this.disposeCallbacks = [];
   }
@@ -25,37 +38,138 @@ class PaperlibEntryScrapeExtension extends PLExtension {
   async initialize() {
     await PLExtAPI.extensionPreferenceService.register(
       this.id,
-      this.defaultPreference
+      this.defaultPreference,
     );
 
-    this.disposeCallbacks.push(
-      PLAPI.hookService.hook("scrapeEntry", this.id, "scrapeEntry")
-    );
+    this.printSomething();
+
+    // 1. Command Extension Example
+    this.registerSomeCommands();
+
+    // 2. UI Extension Example
+    this.modifyPaperDetailsPanel();
+
+    // 3. Hook Extension Example
+    this.hookSomePoints();
+
+    // 4. New Window Extension Example
+    // Please refer to the official doc.
   }
 
   async dispose() {
+    PLExtAPI.extensionPreferenceService.unregister(this.id);
+
     for (const disposeCallback of this.disposeCallbacks) {
       disposeCallback();
     }
-    PLExtAPI.extensionPreferenceService.unregister(this.id);
   }
 
-  async scrapeEntry(payloads: any[]) {
-    console.time("Scrape Entry");
+  printSomething() {
+    console.log("Hello world from extension!");
 
-    if (payloads.length === 0) {
-      console.timeEnd("Scrape Entry");
-      return [];
+    console.log("PLAPI:");
+    for (const key of Object.keys(PLAPI)) {
+      console.log(`  ${key}`);
     }
 
-    const paperEntityDrafts = await this._entryScrapeService.scrape(payloads);
-    console.timeEnd("Scrape Entry");
-    return paperEntityDrafts;
+    console.log("\nPLMainAPI:");
+    for (const key of Object.keys(PLMainAPI)) {
+      console.log(`  ${key}`);
+    }
+
+    console.log("\nPLExtAPI:");
+    for (const key of Object.keys(PLExtAPI)) {
+      console.log(`  ${key}`);
+    }
+  }
+
+  registerSomeCommands() {
+    // When the user choose to run the command, the PLAPI.commandService will
+    // emit a "command_echo_event" event.
+    // we get the message from the preference of this extension by calling PLExtAPI.extensionPreferenceService.get()
+    //
+    this.disposeCallbacks.push(
+      PLAPI.commandService.on("command_echo_event" as any, () => {
+        let msg = PLExtAPI.extensionPreferenceService.get(this.id, "msg");
+        if (PLExtAPI.extensionPreferenceService.get(this.id, "signature")) {
+          if (
+            PLExtAPI.extensionPreferenceService.get(this.id, "lang") === "zh"
+          ) {
+            msg += ` - 来自 SimpleCMD 扩展`;
+          } else {
+            msg += ` - from SimpleCMD Extension`;
+          }
+        }
+
+        PLAPI.logService.info(
+          "Hello from the extension process",
+          msg,
+          true,
+          this.id,
+        );
+      }),
+    );
+
+    // Register a command with event "command_echo_event".
+    this.disposeCallbacks.push(
+      PLAPI.commandService.registerExternel({
+        id: "command_echo",
+        description: "Hello from the extension process",
+        event: "command_echo_event",
+      }),
+    );
+  }
+
+  modifyPaperDetailsPanel() {
+    this.disposeCallbacks.push(
+      PLAPI.uiStateService.onChanged("selectedPaperEntities", (newValues) => {
+        const selectedPaperEntities = newValues.value;
+
+        if (selectedPaperEntities.length === 0) {
+          return;
+        }
+
+        if (selectedPaperEntities.length === 1) {
+          const paperEntity = selectedPaperEntities[0];
+
+          PLAPI.uiSlotService.updateSlot("paperDetailsPanelSlot1", {
+            demo_section_id: {
+              title: "Demo Section",
+              content: `Any string here - ${Math.random()} - ${
+                paperEntity.title
+              }}`,
+            },
+          });
+        }
+      }),
+    );
+  }
+
+  hookSomePoints() {
+    this.disposeCallbacks.push(
+      PLAPI.hookService.hookModify(
+        "scrapeEntryBefore",
+        this.id,
+        "modifyPayloads",
+      ),
+    );
+  }
+
+  modifyPayloads(payloads: any[]) {
+    PLAPI.logService.info("modifyPayloads", `${payloads}`, true, this.id);
+
+    // Modify the payloads here
+    // ...
+
+    // Return the modified payloads
+    // For modify hook, the return value should be an array of args.
+    // For example, the original args array is [payloads: SomeType], then the return value should be also [payloads: SomeType]
+    return [payloads];
   }
 }
 
 async function initialize() {
-  const extension = new PaperlibEntryScrapeExtension();
+  const extension = new PaperlibHelloworldExtension();
   await extension.initialize();
 
   return extension;
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,28 @@
 import commonjs from "@rollup/plugin-commonjs";
-import { rmSync } from "node:fs";
+import { builtinModules } from "module";
 import path from "node:path";
 import modify from "rollup-plugin-modify";
 import { defineConfig } from "vite";
-import { builtinModules } from 'module';
-
-import pkg from "./package.json";
-
-rmSync("dist", { recursive: true, force: true });
-rmSync("release", { recursive: true, force: true });
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    manifest: false,
     minify: false,
     reportCompressedSize: true,
     lib: {
       entry: path.resolve(__dirname, "src/main.ts"),
       fileName: "main",
-      name: "PaperlibEntryScrapeExtension",
+      name: "PaperlibHelloworldExtension",
       formats: ["cjs"],
     },
     rollupOptions: {
-      external: [...builtinModules, "paperlib"],
+      external: [...builtinModules],
       output: {
         format: "cjs",
       },
     },
     outDir: "dist",
+    emptyOutDir: true,
   },
 
   esbuild: {
@@ -41,19 +35,17 @@ export default defineConfig({
     },
   },
 
-  server: process.env.NODE_ENV
-    ? {
-        host: pkg.debug.env.VITE_DEV_SERVER_HOSTNAME,
-        port: pkg.debug.env.VITE_DEV_SERVER_PORT,
-      }
-    : undefined,
-
   plugins: [
     commonjs(),
     modify({
-      find: /import.*from "paperlib";?/,
-      // find: /import { PLAPI } from "paperlib";/,
-      replace: (match, path) => "",
+      find: /import.*from "paperlib-api";?/,
+      replace: (match, path) => {
+        const m = match
+          .replace(/PLAPI\s*,?\s*/g, "")
+          .replace(/PLExtAPI\s*,?\s*/g, "")
+          .replace(/PLMainAPI\s*,?\s*/g, "");
+        return m;
+      },
     }),
   ],
 });
