bab641beea0bf56d4dcb7f1e3cb0a9825a8c59d4
luxiangze <gyk666@email.swu.edu.cn>
2024-06-10 23:07:24 +0800

    the 0.0.1 version extension

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
-import { PLExtAPI, PLExtension } from "paperlib-api/api";
+import { PLAPI, PLExtAPI, PLExtension } from "paperlib-api/api";
+import { PaperEntity } from "paperlib-api/model";
 
-class PaperlibExtension extends PLExtension {
+class PaperlibsciifExtension extends PLExtension {
   disposeCallbacks: (() => void)[];
 
   constructor() {
     super({
-      id: "paperlib-extension",
-      defaultPreference: {},
+      id: "paperlib-sciif-extension",
+      defaultPreference: {
+        key:{
+          type: "string",
+          name: "secretKey",
+          desprition: "The extension alow users to show paper's sciif by setting easyscholar SecretKey.",
+          value: "",
+        },
+      },
     });
 
     this.disposeCallbacks = [];
@@ -15,17 +23,92 @@ class PaperlibExtension extends PLExtension {
   async initialize() {
     await PLExtAPI.extensionPreferenceService.register(
       this.id,
-      this.defaultPreference,
+      this.defaultPreference
+    );
+
+    this.disposeCallbacks.push(
+      PLAPI.uiStateService.onChanged("selectedPaperEntities", (newValues) => {
+        if (newValues.value.length === 1) {
+          this.getPapersci(newValues.value[0]);
+        }
+      })
     );
   }
 
   async dispose() {
+    for (const disposeCallback of this.disposeCallbacks) {
+      disposeCallback();
+    }
+
     PLExtAPI.extensionPreferenceService.unregister(this.id);
+  }
+
+  async getPapersci(paperEntity: PaperEntity) {
+
+    const lang = await PLAPI.preferenceService.get("language");
+
+    const title = lang === "zh-CN" ? "SCI影响因子" : "SCI IF";
+
+
+    await PLAPI.uiSlotService.updateSlot("paperDetailsPanelSlot1", {
+      "pubilicationSCIIF": {
+        title: title,
+        content: `N/A`,
+      },
+    });
+
+    let scrapeURL: string = "";
+    const secretKey = PLExtAPI.extensionPreferenceService.get(this.id, "key");
+    const publication = encodeURIComponent(paperEntity.publication);
+    if (paperEntity.publication !== "") {
+      scrapeURL = `https://www.easyscholar.cc/open/getPublicationRank?secretKey=${secretKey}&publicationName=${publication}`;
+    } 
+
+    try {
+      const response = await PLExtAPI.networkTool.get(
+        scrapeURL,
+        {},
+        1,
+        5000,
+        true,
+        true
+      );
+      const parsedResponse = response.body;
+
+      const pubilicationSCIIF = {
+        sciif: "${parsedResponse.data.offcialRank.all.sciif}",
+        sci: "${parsedResponse.data.offcialRank.all.sci}",
+      };
+
+      PLAPI.uiSlotService.updateSlot("paperDetailsPanelSlot1", {
+        "pubilicationSCIIF": {
+          title: title,
+          content: `${pubilicationSCIIF.sci} (${pubilicationSCIIF.sciif})`,
+        },
+      });
+    } catch (err) {
+      if ((err as Error).message.includes("Key错误")) {
+        PLAPI.logService.warn(
+          "The easyscholar SecretKey is invalid. Please check the key in the extension preference.",
+          "",
+          false,
+          "SCIIFExt"
+        );
+        return;
+      }
+
+      PLAPI.logService.error(
+        "Failed to get sciif.",
+        err as Error,
+        false,
+        "SCIIFExt"
+      );
+    }
   }
 }
 
 async function initialize() {
-  const extension = new PaperlibExtension();
+  const extension = new PaperlibsciifExtension();
   await extension.initialize();
 
   return extension;
